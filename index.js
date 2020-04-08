6580ab4a7a7955a797950cf71511e0052f2f7f6d
adityavj2010 <adityavj2010@gmail.com>
2020-04-09 00:40:19 +0530

    Added mongoose connection

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
 const express = require('express')
 const cors = require('cors')
-const mongoose = require('mongoose')
-const apiRouter = require('./lib/routes/')
-
+const allRoutes = require('./lib/routes')
 const app = express()
+const mongoose = require('mongoose');
+
+const mongoUri = 'mongodb+srv://aditya_cms:qwertyuiop@cluster0-ytf8w.gcp.mongodb.net/test?retryWrites=true&w=majority'
+
+mongoose.connect(mongoUri,{useNewUrlParser:true,useUnifiedTopology:true});
+mongoose.connection.on('error', () => {
+  console.error(`unable to connect to database: ${mongoUri}`);
+});
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }));
 
 app.use(cors())
 
-app.use("/api/", apiRouter);
+app.use(allRoutes);
 
 app.use("*",(req,res)=>{
     return res.status(404).send({message:"Route not found!"})
